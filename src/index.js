e8d83f9fc458878f9e8362cb672b2188ad30e1c6
Daniel Hogan <danielh@airg.com>
2021-05-27 15:04:11 +0100

    remove index.js

--- src/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var express = require("express");
-var app = express();
-var PORT = 8080;
-app.get('/', function (req, res) { return res.send("woot"); });
-app.listen(PORT, function () {
-    console.log("[server]: Server is running at http://localhost:" + PORT);
-});
